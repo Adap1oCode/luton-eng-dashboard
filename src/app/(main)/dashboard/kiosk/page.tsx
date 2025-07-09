f14c0757a04321bbbb5f69a311dead5057717945
Waseem Shazad <github@adaplo.co.uk>
2025-07-09 12:58:26 +0100

    Delete Kiosk folder

--- src/app/(main)/dashboard/kiosk/page.tsx
+++
@@ -1,6 +0,0 @@
-import GenericDashboardPage from '@/components/dashboard/molly-page'
-import { marketingConfig } from '@/app/(main)/dashboard/kiosk/config'
-
-export default function RequisitionsPage() {
-  return <GenericDashboardPage config={marketingConfig} />
-}
